bca397a6cd017c36029cd331225b0d2948ff1997
umut sürmeli <36552511+umutsurmeli@users.noreply.github.com>
2023-08-08 15:43:52 +0300

    core edits
    core/controller - database/db_driver

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -52,8 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link		https://codeigniter.com/userguide3/database/
  */
 abstract class CI_DB_driver {
-
-	/**
+    public $failover;
+    /**
 	 * Data Source Name / Connect string
 	 *
 	 * @var	string
